a01d8750d87bb4517aa89de792a45228ade08b12
Vinusan <severematrixoverload@gmail.com>
2019-04-26 11:03:47 +0100

    Changed sys.stdout to serialPort

--- pong_for_screen.py
+++ pong_for_screen.py
@@ -13,13 +13,13 @@ if (serialPort.isOpen() == False):
 
 
 def blue():
-	sys.stdout.write(u"\u001b[46m \u001b[0m")
+	serialPort.write(u"\u001b[46m \u001b[0m")
 
 def grey():
-	sys.stdout.write(u"\u001b[47m \u001b[0m") 		
+	serialPort.write(u"\u001b[47m \u001b[0m") 		
 
 def red():
-	sys.stdout.write(u"\u001b[41m \u001b[0m") 												
+	serialPort.write(u"\u001b[41m \u001b[0m") 												
 
 def startScreen():
 
