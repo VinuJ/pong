f844c3e16092bc8d65cade778402d0574a376635
Derick <derick.ee96@gmail.com>
2019-04-26 12:06:38 +0100

    Update adcTest.py

--- adcTest.py
+++ adcTest.py
@@ -9,8 +9,9 @@ bus = smbus.SMBus(1)
 
 bus.write_byte( I2CADDR, CMD_CODE ) 
 tmp = bus.read_word_data( I2CADDR, 0x00 ) 
-print(tmp)
+
+print(tmp) # before bit manipulation (for checking, remove when finalising)
 
 tmp = ( ( ( (tmp << 8) + (tmp >> 8) ) | 0xFFF000 ) ^ 0xFFF000 )
 
-print(tmp)
+print(tmp) # after bit manipulation (true value)
