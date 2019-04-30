0e4134580da78318d8c33584572220407b3f6868
Vinusan <severematrixoverload@gmail.com>
2019-04-30 16:36:36 +0100

    Updated ball movement

--- pong_for_screen.py
+++ pong_for_screen.py
@@ -5,21 +5,62 @@ import time
 
 
 # Open Pi serial port, speed 9600 bits per second
-serialPort = Serial("/dev/ttyAMA0", 9600)
+serialPort = Serial("/dev/ttyAMA0", 115200)
 # Should not need, but just in case
 
 if (serialPort.isOpen() == False):
 	serialPort.open()
 
+# Colours
 
 def blue():
-	serialPort.write(u"\u001b[46m \u001b[0m")
+	serialPort.write(u"\u001b[46m \u001b[0m") 
+	serialPort.write(u"\u001b[1D")
 
 def grey():
 	serialPort.write(u"\u001b[47m \u001b[0m") 		
+	serialPort.write(u"\u001b[1D")
 
 def red():
-	serialPort.write(u"\u001b[41m \u001b[0m") 												
+	serialPort.write(u"\u001b[41m \u001b[0m")
+	serialPort.write(u"\u001b[1D")
+
+# Cursor Navigation	
+
+def c_up(steps):
+	for i in range(0, steps):
+		serialPort.write(u"\u001b[1A")
+		
+def c_down(steps):
+	for i in range(0, steps):
+		serialPort.write(u"\u001b[1B")
+		
+def c_right(steps):
+	for i in range(0, steps):
+		serialPort.write(u"\u001b[1C")
+		
+def c_left(steps):
+	for i in range(0, steps):
+		serialPort.write(u"\u001b[1D")
+		
+# Shift Cursor And Fill
+
+def shiftAndFill(shiftx, shifty, colour):
+	c_right(shiftx)
+	c_up(shifty)
+	colour()
+	c_left(shiftx)
+	c_down(shifty)
+
+# Ball Functions
+
+def drawBall(oldx, oldy, newx, newy):
+	
+	shiftAndFill(oldx, oldy, grey)
+	
+	shiftAndFill(newx, newy, red)	
+		
+# Starting Screen
 
 def startScreen():
 
@@ -27,18 +68,56 @@ def startScreen():
     		for j in range(0, 80):	
 			nets = [0,1,4,5,8,9,12,13,16,17,20,21]
 			
-			if (j == 2 or j == 77) and (i >= 11 and i <= 13): 					# paddles
+			if (j == 2 or j == 77) and (i >= 11 and i <= 13): # paddles
 				blue()
-			elif (j == 39) and (i in nets): 							# nets
+			# elif (j == 39) and (i in nets): # nets
 				blue()	
-			elif (j == 3) and (i == 12):								# ball
+			# elif (j == 3) and (i == 12): # ball
 				red()	
-			else:											# background
+			else: # background
 				grey()		
+				
+# Ball Class
 
+class Ball:
+	def _init_(self, oldx, oldy, newx, newy):
+		self.oldx = oldx
+		self.oldy = oldy
+		self.newx = newx
+		self.newy = newy	
 startScreen()
 
+c_up() # move to (0, 0)
+playerServe = 1 # which player is serving
+
+if (playerServe == 1):
+	ball = Ball(3, 12, 3, 12)
+if (playerServe == 2):
+	ball = Ball(20, 12)
+
+while(True): # constant loop for ball movement
+	drawBall(ball.oldx, ball.oldy, ball.newx, ball.newy) # draw ball
+	
+	ball.oldx = ball.newx
+	ball.oldy = ball.newy
+	
+	if (playerServe == 1): # if player 1 serving, direction of ball is down-right
+		ball.newx += 1
+		ball.newy -= 1
+		
+		
+	if (playerServe == 2): # if player 2 is serving, direction of ball is down-left
+		ball.newx -= 1
+		ball.newy -= 1
+		
+		
+	time.sleep(1)
+
+		
+
+
 serialPort.close()
+
 	
 
 
